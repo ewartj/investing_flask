639e97c51e8298f6f9eef146e685b5f3a09b065e
ewartj <ewart.jonny@gmail.com>
2022-07-25 22:32:45 +0100

    refined the basic calculations based on the little book of investing

--- basic_wacc.py
+++ basic_wacc.py
@@ -5,10 +5,11 @@ Created on Sun Jul 24 13:41:06 2022
 
 @author: jonny
 """
+from pandas import read_pickle
 import yahoo_fin.stock_info as stock_info
 import numpy as np
 
-ticker = "HOTC.L"
+ticker = "SHEL.L"
 
 df = stock_info.get_balance_sheet(ticker)
 df = df.replace(np.nan, 0)
@@ -25,8 +26,11 @@ current_assets_list = df.loc["totalCurrentAssets", :].values.tolist()
 current_assets = current_assets_list[0]
 print(f"current_assets {current_assets}")
 
-goodwill_list = df.loc["goodWill", :].values.tolist()
-goodwill = goodwill_list[0]
+try:
+    goodwill_list = df.loc["goodWill", :].values.tolist()
+    goodwill = goodwill_list[0]
+except:
+    goodwill = 0.0
 print(f"goodwill {goodwill}")
 
 
@@ -90,18 +94,28 @@ print(f"capex {capex}")
 
 #FCFF
 
-working_capital = (current_assets_list[0] - current_liabilities_list[0]) - (current_assets_list[1] - current_liabilities_list[1])
-y = current_assets_list[0] - current_liabilities_list[0]
-z = current_assets_list[1] - current_liabilities_list[1]
-print(y)
-print(z)
+try:
+    working_capital = (current_assets_list[1]  - current_liabilities_list[1]) - (current_assets_list[0] - current_liabilities_list[0])
+    y = current_assets_list[0] - current_liabilities_list[0]
+    z = current_assets_list[1] - current_liabilities_list[1]
+    print(y)
+    print(z)
+except:
+    working_capital = current_assets_list[0]- current_liabilities_list[0]
+
 
 #working_capital=Default_Spread = float(input("Enter working capital: ")) #for when theres only one number in the list
 #working_capital=working_capital * 1000
 print("working capital")
 print(working_capital)
 
-FCFF = net_income_list[0] - (abs(capex_list[0]) + working_capital)
+# FCFF = net_income_list[0] - (abs(capex_list[0]) + working_capital)
+
+reinvestment_rate = (abs(capex_list[0]) + working_capital) / net_income_list[0]
+FCFF = net_income_list[0] * (100 - reinvestment_rate)
+
+print('Reinvestment rate =')
+print(reinvestment_rate)
 
 print('FCFF=')
 print(FCFF)
@@ -110,12 +124,16 @@ print(FCFF)
 df3 = stock_info.get_income_statement(ticker)
 df3 = df3.replace(np.nan, 0)
 
+basic_stats = stock_info.get_quote_table(ticker)
+
+shares = basic_stats['Volume']
+
 interest_list = df3.loc["interestExpense", :].values.tolist()
 interest = abs(interest_list[0])
 print(f"interest {interest}")
 
 if interest == 0:
-    interest_coverage = 13
+    interest_coverage = 13 # why this- ITS above the default spread (check still correct)
 else:
     interest_coverage = net_income_list[0] / interest
 
@@ -126,80 +144,69 @@ metrics_dict = stock_info.get_quote_table(ticker)
 beta = metrics_dict["Beta (5Y Monthly)"]
 print(f"beta {beta}")
 
-default_spread = 0
+default_spread = 0 # Probably need to update these
 if interest_coverage >= 12.5:
-     default_spread = 0.006
+     default_spread = 0.67
 elif interest_coverage < 12.499 and interest_coverage >= 9.5:
-     default_spread = 0.008
-elif interest_coverage < 9.499 and interest_coverage >= 7.5:
-     default_spread = 0.01
+     default_spread = 0.82
+elif interest_coverage < 9.499 and interest_coverage >= 3:
+     default_spread = 1.03
 elif interest_coverage < 7.499 and interest_coverage >= 6:
-     default_spread = 0.011
+     default_spread = 1.14
 elif interest_coverage < 5.999 and interest_coverage >= 4.5:
-     default_spread = 0.0125
+     default_spread = 1.29
 elif interest_coverage < 4.499 and interest_coverage >= 4:
-     default_spread = 0.016
+     default_spread = 1.59
 elif interest_coverage < 3.999 and interest_coverage >= 3.5:
-     default_spread = 0.025
+     default_spread = 1.93
 elif interest_coverage < 3.499 and interest_coverage >= 3:
-     default_spread = 0.03
-elif interest_coverage < 9.499 and interest_coverage >= 7.5:
-     default_spread = 0.01
-elif interest_coverage < 9.499 and interest_coverage >= 7.5:
-     default_spread = 0.01
-elif interest_coverage < 7.499 and interest_coverage >= 6:
-     default_spread = 0.011
-elif interest_coverage < 5.999 and interest_coverage >= 4.5:
-     default_spread = 0.0125
-elif interest_coverage < 4.499 and interest_coverage >= 4:
-     default_spread = 0.016
-elif interest_coverage < 3.999 and interest_coverage >= 3.5:
-     default_spread = 0.025
-elif interest_coverage < 3.499 and interest_coverage >= 3:
-     default_spread = 0.03
+     default_spread = 2.15
 elif interest_coverage < 2.999 and interest_coverage >= 2.5:
-     default_spread = 0.0375
+     default_spread = 3.15
 elif interest_coverage < 2.499 and interest_coverage >= 2:
-     default_spread = 0.045
+     default_spread = 3.78
 elif interest_coverage < 1.999 and interest_coverage >= 1.5:
-     default_spread = 0.055
+     default_spread = 4.62
 elif interest_coverage < 1.499 and interest_coverage >= 1.25:
-     default_spread = 0.065
+     default_spread = 7.76
 elif interest_coverage < 1.249 and interest_coverage >= 0.8:
-     default_spread = 0.08
+     default_spread = 8.8
 elif interest_coverage < 0.799 and interest_coverage >= 0.5:
-     default_spread = 0.105
+     default_spread = 10.76
 else:
-     default_spread = 0.14
+     default_spread = 14.34
 
 #Default_Spread = float(input("Enter Default Spread: "))
 print(f"default spread {default_spread}")
 
-risk_free_rate = 0.73
-equity_risk_premium = 1.06
-equity = retained_earnings_list[0] + common_stock_list[0] + capital_surplus_list[0]
+risk_free_rate = 1.25 # this is UK BoE interest rate can I get this from an API
+equity_risk_premium = 6 # is ther an API to get this?
+# equity = retained_earnings_list[0] + common_stock_list[0] + capital_surplus_list[0]
+share_price = stock_info.get_live_price(ticker)
+equity = share_price * shares
+print(f"equity {equity}")
 debt = short_term_debt + long_term_debt
 
 cost_of_equity = risk_free_rate + beta * equity_risk_premium
 print('Cost of Equity =')
 print(cost_of_equity)
 
-tax = 0.2
+tax = 0.20 # put in config
 
-cost_of_debt = (risk_free_rate + default_spread) * 0.8
+cost_of_debt = (risk_free_rate + default_spread) * (1 - tax)
 print('Cost of Debt =')
 print(cost_of_debt)
 
 equity_and_debt = equity + debt
 
 
-weighed_d = float(debt) / equity_and_debt
+weighed_d = cost_of_debt * (float(debt) / equity_and_debt)
 
 
-weighed_c = float(equity) / equity_and_debt
+weighed_e = cost_of_equity * (float(equity) / equity_and_debt)
 
 
-weighed_capital = float(cost_of_equity) * weighed_c
+weighed_capital = weighed_e + weighed_d #float(cost_of_equity) * weighed_c
 print('Weighted Cost of Capital =')
 print(weighed_capital)
 
@@ -207,7 +214,7 @@ weighed_debt = float(cost_of_debt) * weighed_d
 print('Weighted Cost of Debt =')
 print(weighed_debt)
 
-cost_of_capital = float(weighed_capital) + float(weighed_debt)
+cost_of_capital = (float(weighed_capital) + float(weighed_debt)) / 100
 print('Cost of Capital =')
 print(cost_of_capital)
 
@@ -216,69 +223,122 @@ print(cost_of_capital)
 todays_income = net_income_list[0]
 
 
-reinvestment_rate = (float(retained_earnings_list[0]) / float(net_income_list[0]))
-reinvestment_rate = float(reinvestment_rate) / 100
-reinvestment_rate = abs(reinvestment_rate)
-print('Reinvestment rate =')
-print(reinvestment_rate)
+
+
+# reinvestment_rate = (float(retained_earnings_list[0]) / float(net_income_list[0])) # is this correct?
+# reinvestment_rate = float(reinvestment_rate) / 100
+# reinvestment_rate = abs(reinvestment_rate)
+
 
 gross_profit_list = df3.loc["grossProfit", :].values.tolist()
 def calculate_growth_rate(gross_profit_list):
-    two_yrs_ago = gross_profit_list[2] / gross_profit_list[3]
-    one_yr_ago = gross_profit_list[1] / gross_profit_list[2]
-    this_yr= gross_profit_list[0] / gross_profit_list[3]
+    two_yrs_ago = (gross_profit_list[2] - gross_profit_list[3]) / gross_profit_list[3]
+    one_yr_ago = (gross_profit_list[1] - gross_profit_list[2]) / gross_profit_list[2]
+    this_yr= (gross_profit_list[0] - gross_profit_list[1]) / gross_profit_list[1]
     avg = (two_yrs_ago + one_yr_ago + this_yr) / 3
     return avg
 
 growth_rate = calculate_growth_rate(gross_profit_list)
+print(growth_rate)
 
-
-growth_rate = float(growth_rate) / 100
+# growth_rate = float(growth_rate) * 100
 print(f"growth rate {growth_rate}")
 
-operating_earnings = net_income_list[0]
-#growth_rate = growth_rate
-delta = 2 #int(input("How many years would you like to look ahead? 3[1] or 5[2] or 10[3] "))
-#delta = delta - 1
-year = 0
-total = 0
+# Calculating terminal value
 
-while year <= delta:
-    year = delta + 1
-#1
-    o1 = operating_earnings + (operating_earnings *(1 + growth_rate))
-#    o2 = o1 + (o1 - (o1 * float(Reinvestment_rate)))*(1 + growth_rate)
-    o2 = o1 + (o1 *(1 + growth_rate))
-    o3 = o2 + (o2 *(1 + growth_rate))
-    o4 = o3 + (o3 *(1 + growth_rate))
-    o5 = o4 + (o4 *(1 + growth_rate))
-    o6 = o5 + (o5 *(1 + (growth_rate)))
-    o7 = o6 + (o6 *(1 + (growth_rate)))
-    o8 = o7 + (o7 *(1 + (growth_rate)))
-    o9 = o8 + (o8 *(1 + (growth_rate)))
-    o10 = o9 + (o9 * (1 + (growth_rate)))
-    operating_earnings = o10 * (1 + (growth_rate))
-    pupils_dictionary = {}
+roc = net_income_list[0] / (total_assets_list[0] - current_liabilities_list[0])
+print(f"ROC = {roc}")
+
+
+operating_earnings = net_income_list[0]
+
+years = 5
+
+def pred_future_growth(operating_earnings, growth_rate, reinvestment_rate, years):
+    future_income_lst = [operating_earnings]
+    reinvestment_lst = []
+    fcff_lst = []
+    if len(future_income_lst) == 1:
+        op_inc = operating_earnings * (1 + growth_rate)
+        reinv = op_inc * reinvestment_rate
+        fcff = op_inc - reinv
+        future_income_lst.append(op_inc)
+        reinvestment_lst.append(reinv)
+        fcff_lst.append(fcff)
+    for year in range(years - 1):
+        op_inc = future_income_lst[-1] * (1 + growth_rate)
+        reinv = reinvestment_lst[-1] * reinvestment_rate
+        fcff = op_inc - reinv
+        future_income_lst.append(op_inc)
+        reinvestment_lst.append(reinv)
+        fcff_lst.append(fcff)
+    future_earnings = {
+                        "income": future_income_lst,
+                        "reinvestment": reinvestment_lst,
+                        "fcff": fcff_lst
+                        }
+    return future_earnings
+
+reinvestment_rate = reinvestment_rate / 100
+
+future_earnings = pred_future_growth(operating_earnings, growth_rate, reinvestment_rate, years)
+print(future_earnings)
+
+terminal_cashflow = future_earnings["fcff"][-1]
+
+stable_reinv_rate = (growth_rate / roc) / 100
+print(f"stable_reinv_rate {stable_reinv_rate}")
+
+terminal_value = terminal_cashflow * (1 + growth_rate) * (1 - stable_reinv_rate) / cost_of_capital - growth_rate
+
+discounted_terminal_value = (future_earnings["fcff"][1] / (1 + cost_of_capital)) + (future_earnings["fcff"][2] / (1 + cost_of_capital)) + (future_earnings["fcff"][3] / (1 + cost_of_capital))  
++ (future_earnings["fcff"][4] + terminal_value) / (1 + cost_of_capital)
+
+
+# #growth_rate = growth_rate
+# delta = 2 #int(input("How many years would you like to look ahead? 3[1] or 5[2] or 10[3] "))
+# #delta = delta - 1
+# year = 0
+# total = 0
+
+# while year <= delta:
+#     year = delta + 1
+# #1
+#     o1 = operating_earnings + (operating_earnings *(1 + growth_rate))
+# #    o2 = o1 + (o1 - (o1 * float(Reinvestment_rate)))*(1 + growth_rate)
+#     o2 = o1 + (o1 *(1 + growth_rate))
+#     o3 = o2 + (o2 *(1 + growth_rate))
+#     o4 = o3 + (o3 *(1 + growth_rate))
+#     o5 = o4 + (o4 *(1 + growth_rate))
+#     o6 = o5 + (o5 *(1 + (growth_rate)))
+#     o7 = o6 + (o6 *(1 + (growth_rate)))
+#     o8 = o7 + (o7 *(1 + (growth_rate)))
+#     o9 = o8 + (o8 *(1 + (growth_rate)))
+#     o10 = o9 + (o9 * (1 + (growth_rate)))
+#     operating_earnings = o10 * (1 + (growth_rate))
+#     pupils_dictionary = {}
 #    for x in year(delta):
 #        new_key = year + 1
 #        new_age = operating_earnings
 #        pupils_dictionary[new_key] = new_age
-    year = delta + 1
+    # year = delta + 1
 
-if delta == 1:
-    total = o1 + o2 + o3
-elif delta == 2:
-    total = o1 + o2 + o3 + o4 + o5
-else:
-    total = o1 + o2 + o3 + o4 + o5 + o6 + o7 + o8 + o9 + o10
+# if delta == 1:
+#     total = o1 + o2 + o3
+# elif delta == 2:
+#     total = o1 + o2 + o3 + o4 + o5
+# else:
+#     total = o1 + o2 + o3 + o4 + o5 + o6 + o7 + o8 + o9 + o10
 
-print(total)
+# print(total)
+# reinvestment_rate = reinvestment_rate / 100
+# terminal_value = (float(operating_earnings * (1 - reinvestment_rate))) / (float(cost_of_capital - growth_rate))
 
-terminal_value = (float(operating_earnings * (1 - reinvestment_rate))) / (float(cost_of_capital - growth_rate))
+# total = total + terminal_value
+# print("terminal value:")
+# print(f"total {total}")
 
-total = total + terminal_value
-print("terminal value:")
-print(f"total {total}")
+total = discounted_terminal_value + cash_list[0] - (short_term_debt + long_term_debt)
 
 # Minority
 try:
@@ -292,10 +352,12 @@ if minority > 0.000001:
     total = total - minority_list[0]
 
 value_company = total + cash_list[0]
+print(value_company)
 
 if debt > 0.000001:
     value_company = value_company - debt
 
+value_company = value_company / 100 #yahoo does thinsg in cents and pence
 #value_company  = abs(value_company)
 
 print("value of Company")
@@ -308,10 +370,6 @@ print(value_company )
 #     Value_Company = Value_Company / 1000000
 # if size ==
 #     Value_Company = Value_Company / 1000000000
-
-basic_stats = stock_info.get_quote_table(ticker)
-
-shares = basic_stats['Volume']
 
 
 value_per_share = float(value_company) / shares
